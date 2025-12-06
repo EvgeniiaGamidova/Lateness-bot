f775eaa088060efb6407af837ba6fdb7d3a42bb7
Evgeniia Chernousova <129880747+EvgeniiaGamidova@users.noreply.github.com>
2025-12-06 20:17:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { Bot, GrammyError, HttpError } = require('grammy');
 
-const bot = new Bot(BOT_API_KEY);
+const bot = new Bot(process.env.BOT_API_KEY);
 
 bot.api.setMyCommands([
   {
